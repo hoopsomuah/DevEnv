2a9a67fecf5ef07fcb06dd695f0125ed61f20443
Hoop Somuah <hoop@somuah.com>
2023-06-13 18:25:09 -0400

    making setup script more robust

--- setup.ps1
+++ setup.ps1
@@ -1,22 +1,88 @@
-if (!(Get-Module posh-git -ListAvailable)) {
-    $installPoshGit = Read-Host "The posh-git module is not installed. Do you want to install it? (Y/N)"
-    if ($installPoshGit -eq "Y") {
+<#
+.SYNOPSIS
+    Setup script for a new machine
+.DESCRIPTION
+    This script will install all the apps and tools I use on a new machine.
+    It will also configure the machine to my preferences.
+.PARAMETER reLaunched
+    This parameter is used to indicate that the script has been relaunched in elevated mode.
+
+#>
+[CmdletBinding()]
+param(
+    [switch]$reLaunched
+)
+
+if ($reLaunched) {
+    Write-Host "Relaunched in elevated mode..."
+}
+switch ($VerbosePreference) {
+    'SilentlyContinue' { Write-Host "Normal Mode" }
+    'Continue' { Write-Host "Diagnostic Mode" }
+    'Stop' { Write-Host "Full Diagnostic Mode" }
+}
+
+function Confirm-Action {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Message
+    )
+    do {
+        $response = Read-Host "$Message (Y/N)"
+    } while ($response -notmatch "^[yn]$")
+
+    return $response -eq "y"
+}
+
+try {
+
+    # check if we are running as admin
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        if(Confirm-Action "This script must be run as Administrator.  Restart in elevated mode?")
+        {
+            $pwshArgs = @("-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "$PSCommandPath", "-reLaunched") + $args
+            Start-Process -FilePath pwsh.exe -ArgumentList $pwshArgs -Verb RunAs -Wait
+        }
+        exit
+    }
+
+    if (!(Get-Module posh-git -ListAvailable)) {
         Install-Module posh-git -Scope CurrentUser -Force 
     }
-}
 
-if (!(Get-Module PSReadLine -ListAvailable)) {
-    $installPSReadLine = Read-Host "The PSReadLine module is not installed. Do you want to install it? (Y/N)"
-    if ($installPSReadLine -eq "Y") {
+    if (!(Get-Module PSReadLine -ListAvailable)) {
         Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force
     }
-}
 
-if (!(winget list -e JanDeDobbeleer.OhMyPosh)) {
-    $installOhMyPosh = Read-Host "Oh My Posh is not installed. Do you want to install it using Winget? (Y/N)"
-    if ($installOhMyPosh -eq "Y") {
-        winget install JanDeDobbeleer.OhMyPosh
+    $wingetArgs = switch ($VerbosePreference) {
+        'SilentlyContinue' { '--disable-interactivity' }
+        'Continue' { '--verbose' }
+        'Stop' { '--verbose' }
+        default { '--verbose' }
+    }
+
+    &winget.exe configure -f $PSScriptRoot\min-dev.dsc.yaml $wingetArgs
+
+    if (Confirm-Action "Install all dev apps?") {
+        &winget.exe configure -f $PSScriptRoot\dev-plus.dsc.yaml $wingetArgs
+    }
+
+    #validate that we are in the git repo and not running this script from a download.
+    if((git rev-parse --is-inside-work-tree) -ne 'true'){
+
+    }
+
+    if ((Test-Path $Profile.CurrentUserAllHosts) -and -not(Confirm-Action "Overwrite Existing Profile?"))
+    {
+        exit
+    }
+
+    # copy the contents of the bootstrap folder to the users powershell script folder
+    Copy-Item -Path "$PSScriptRoot\bootstrap\profile.ps1" -Destination $Profile.CurrentUserAllHosts -Recurse -Force
+}
+finally {
+    if ($reLaunched) {
+        Read-Host "Setup Complete. Press any key to continue..."
     }
 }
-# copy the contents of the bootstrap folder to the users powershell script folder
-Copy-Item -Path "$PSScriptRoot\bootstrap\profile.ps1" -Destination $Profile.CurrentUserAllHosts -Recurse -Force
